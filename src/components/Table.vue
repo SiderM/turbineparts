bf0f912c4233845621e84c011c5c251ea65389b9
Maksim Gavrilov <gavrilovmaksim91@gmail.com>
2020-12-28 22:55:19 +0300

    clear other in project

---
+++ src/components/Table.vue
@@ -0,0 +1,93 @@
+<template>
+  <table class="table table-hover">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+    export default {
+        name: "Table"
+    }
+</script>
+
+<style scoped>
+  tr {
+    width: 100%;
+    display: inline-table;
+    table-layout: fixed;
+  }
+
+  tbody {
+    overflow-y: scroll;
+    height: 70vh;
+    position: absolute;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Home Page
+    {{process.env.MONGO_URL}}
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
