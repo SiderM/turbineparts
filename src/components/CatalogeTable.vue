7434a2797fbbcd276ddc2e512f5ccb0b40088277
Maksim Gavrilov <gavrilovmaksim91@gmail.com>
2021-01-04 00:14:11 +0300

    add loading snippet

--- src/components/CatalogeTable.vue
+++ src/components/CatalogeTable.vue
@@ -1,25 +1,31 @@
 <template>
-  <table class="table table-hover">
-    <thead>
-    <tr>
-      <th scope="col">№ Детлаи</th>
-      <th scope="col" colspan="3">Наименование</th>
-      <th scope="col">Цена (USD)</th>
-      <th scope="col">Цена (курс ЦБ)</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="part in filteredParts" :key="part._id.$oid">
-      <th scope="row" v-if="part.id !== null">{{part.id}}</th>
-      <th scope="row" v-else>Нет номера</th>
-      <td colspan="3">{{part.title}}</td>
-      <td v-if="part.price !== null">{{part.price}} $</td>
-      <td v-else>Нет цены</td>
-      <td v-if="part.price !== null">{{parseInt(Number(part.price) / usd)}} руб.</td>
-      <td v-else>Нет цены</td>
-    </tr>
-    </tbody>
-  </table>
+  <div>
+    <div class="d-flex align-items-center" v-if="isLoading">
+      <strong>Получение данных...</strong>
+      <div class="spinner-border ms-auto" style="width: 4rem; height: 4rem" role="status" aria-hidden="true"></div>
+    </div>
+    <table v-else class="table table-hover">
+      <thead>
+      <tr>
+        <th scope="col">№ Детлаи</th>
+        <th scope="col" colspan="3">Наименование</th>
+        <th scope="col">Цена (USD)</th>
+        <th scope="col">Цена (курс ЦБ)</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="part in filteredParts" :key="part._id.$oid">
+        <th scope="row" v-if="part.id !== null">{{part.id}}</th>
+        <th scope="row" v-else>Нет номера</th>
+        <td colspan="3">{{part.title}}</td>
+        <td v-if="part.price !== null">{{part.price}} $</td>
+        <td v-else>Нет цены</td>
+        <td v-if="part.price !== null">{{parseInt(Number(part.price) / usd)}} руб.</td>
+        <td v-else>Нет цены</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -36,9 +42,9 @@
         data() {
             return {
                 parts: [],
-                usd: Number
+                usd: Number,
+                isLoading: true
             }
-
         },
         computed: {
             filteredParts() {
@@ -48,14 +54,20 @@
         methods: {
             getUSD() {
                 this.axios.get('https://www.cbr-xml-daily.ru/latest.js').then(response => this.usd = response.data.rates.USD)
-            }
+            },
+            getParts() {
+                this.axios.get(`${process.env.BASE_URL}parts/${this.category}.json`).then(response => {
+                    this.isLoading = false
+                    return this.parts = response.data
+                })
 
+            }
         },
         created() {
             this.getUSD()
         },
         mounted() {
-            this.axios.get(`${process.env.BASE_URL}parts/${this.category}.json`).then(response => this.parts = response.data)
+            this.getParts()
         }
     }
 </script>
--- src/views/Instructions.vue
+++ src/views/Instructions.vue
@@ -1,27 +1,12 @@
 <template>
   <div>
     <div>Instructions Page</div>
-    <div>{{usd}}</div>
   </div>
 </template>
 
 <script>
     export default {
-        name: "Instructions",
-        data() {
-            return {
-                usd: Number
-            }
-        },
-        methods: {
-            getUSD() {
-                this.axios.get('https://www.cbr-xml-daily.ru/latest.js').then(response => this.usd = response.data.rates.USD)
-            }
-
-        },
-        mounted() {
-            this.getUSD()
-        }
+        name: "Instructions"
     }
 </script>
 
